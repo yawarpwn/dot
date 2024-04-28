dbadccdd167cc95ff38afee7ba27aab9b8b2955b
yawarpwn <john.kazz@gmail.com>
2024-04-27 22:04:37 -0400

    add readme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,31 +7,31 @@ require("keys").setup(config)
 require("links").setup(config)
 
 -- WSL.exe
-local wsl_domains = wezterm.default_wsl_domains()
-for idx, domain in ipairs(wsl_domains) do
-	domain.default_cwd = "~"
-end
+-- local wsl_domains = wezterm.default_wsl_domains()
+-- for idx, domain in ipairs(wsl_domains) do
+-- 	domain.default_cwd = "~"
+-- end
 
 -- wezterm.on("gui-startup", function(cmd)
 -- 	local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
 -- 	window:gui_window():toggle_fullscreen()
 -- end)
-config.default_domain = "WSL:Debian"
-config.set_environment_variables = {
-	-- This changes the default prompt for cmd.exe to report the
-	-- current directory using OSC 7, show the current time and
-	-- the current directory colored in the prompt.
-	prompt = "$E]7;file://localhost/$P$E\\$E[32m$T$E[0m $E[35m$P$E[36m$_$G$E[0m ",
-}
+-- config.default_domain = "WSL:Debian"
+-- config.set_environment_variables = {
+-- 	-- This changes the default prompt for cmd.exe to report the
+-- 	-- current directory using OSC 7, show the current time and
+-- 	-- the current directory colored in the prompt.
+-- 	prompt = "$E]7;file://localhost/$P$E\\$E[32m$T$E[0m $E[35m$P$E[36m$_$G$E[0m ",
+-- }
 
-config.front_end = "WebGpu"
-config.webgpu_power_preference = "HighPerformance"
-config.animation_fps = 1
-config.cursor_blink_ease_in = "Constant"
-config.cursor_blink_ease_out = "Constant"
-config.underline_thickness = 3
-config.cursor_thickness = 4
-config.underline_position = -6
+-- config.front_end = "WebGpu"
+-- config.webgpu_power_preference = "HighPerformance"
+-- config.animation_fps = 1
+-- config.cursor_blink_ease_in = "Constant"
+-- config.cursor_blink_ease_out = "Constant"
+-- config.underline_thickness = 3
+-- config.cursor_thickness = 4
+-- config.underline_position = -6
 
 --Colorscheme
 config.color_scheme = "Catppuccin Mocha"
---
+++ README.md
@@ -0,0 +1,5 @@
+```shell
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+```
