4359f8ddfee8297144292c917b99577f6ffe3fff
yawarpwn <john.kazz@gmail.com>
2024-07-08 13:33:18 -0500

    update nvim config

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,22 +1,3 @@
-local util = require("util")
-
-util.wezterm()
-
-local keymap = vim.keymap
-local opts = { noremap = true, silent = true }
-
--- Disabled keymaps
-keymap.set("n", "<leader>l", "")
-keymap.set("n", "<leader>-", "")
-keymap.set("n", "<leader>|", "")
-keymap.set("n", "<leader>`", "")
-
--- Disable continuations
-keymap.set("n", "<Leader>o", "o<Esc>^Da", opts)
-keymap.set("n", "<Leader>O", "O<Esc>^Da", opts)
-
---Lsp
-keymap.set("n", "<leader>lr", "<cmd>:LspRestart<cr>", { desc = "Restart LSP" })
-
---Change word with <c-c>
-keymap.set("n", "<c-c>", "<cmd>normal! ciw<cr>a")
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
---
+++ .config/nvim_bak/lua/config/keymaps.lua
@@ -0,0 +1,22 @@
+local util = require("util")
+
+util.wezterm()
+
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Disabled keymaps
+keymap.set("n", "<leader>l", "")
+keymap.set("n", "<leader>-", "")
+keymap.set("n", "<leader>|", "")
+keymap.set("n", "<leader>`", "")
+
+-- Disable continuations
+keymap.set("n", "<Leader>o", "o<Esc>^Da", opts)
+keymap.set("n", "<Leader>O", "O<Esc>^Da", opts)
+
+--Lsp
+keymap.set("n", "<leader>lr", "<cmd>:LspRestart<cr>", { desc = "Restart LSP" })
+
+--Change word with <c-c>
+keymap.set("n", "<c-c>", "<cmd>normal! ciw<cr>a")
