ae792ad1429acd15716138c0d0b4c23bcd203440
yawarpwn <john.kazz@gmail.com>
2024-07-10 11:28:50 -0500

    update wezterm

--- .config/nvim/test/javascript.js
+++ .config/nvim/test/javascript.js
@@ -5,4 +5,22 @@ const leg = (name) => {
   };
 };
 
-const indent_value = "name";
+const name = "name";
+
+const sum = () => {
+  const value =[10, 20, 30, 40] 
+}
+
+class Foo {
+  geName = function bar() {
+    let obj  = {}
+    
+  }
+}
+
+
+"suelto"
+
+for (const iterator of object) {
+ console.log("get") 
+}
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -44,8 +44,8 @@ else
 end
 -- Fonts
 config.font_size = 11
--- config.font = wezterm.font({ family = "Fira Code Nerd Font" })
-config.font = wezterm.font({ family = "JetBrains Mono" })
+config.font = wezterm.font({ family = "Fira Code Nerd Font" })
+-- config.font = wezterm.font({ family = "JetBrains Mono" })
 config.bold_brightens_ansi_colors = true
 
 -- Cursor
