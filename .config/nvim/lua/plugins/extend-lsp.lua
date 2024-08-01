6923d09bcbe0dce6a3c40e4d7e07c7777cadbab4
johneyder <john.kazz@gmail.com>
2024-08-01 11:56:51 -0500

    update

--- .config/nvim/lua/plugins/extend-lsp.lua
+++ .config/nvim/lua/plugins/extend-lsp.lua
@@ -6,25 +6,11 @@ return {
       vtsls = {
         -- explicitly add default filetypes, so that we can extend
         settings = {
-          complete_function_calls = true,
-          vtsls = {
-            enableMoveToFileCodeAction = true,
-            autoUseWorkspaceTsdk = true,
-            experimental = {
-              completion = {
-                enableServerSideFuzzyMatch = true,
-              },
-            },
-          },
           typescript = {
-            updateImportsOnFileMove = { enabled = "always" },
-            suggest = {
-              completeFunctionCalls = true,
-            },
             inlayHints = {
               enumMemberValues = { enabled = false },
               functionLikeReturnTypes = { enabled = false },
-              parameterNames = { enabled = "literals" },
+              parameterNames = { enabled = false },
               parameterTypes = { enabled = false },
               propertyDeclarationTypes = { enabled = false },
               variableTypes = { enabled = false },
