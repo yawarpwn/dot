63c81518dc33eb608e62a64cc79e35f44e567824
yawarpwn <john.kazz@gmail.com>
2024-08-24 09:40:33 -0500

    update keys in wezterm

--- .config/nvim/lua/plugins/temp.lua
+++ .config/nvim/lua/plugins/temp.lua
@@ -13,33 +13,4 @@ return {
   opts = {
     threshold = 4,
   },
-  {
-    "letieu/wezterm-move.nvim",
-    keys = { -- Lazy loading, don't need call setup() function
-      {
-        "<C-h>",
-        function()
-          require("wezterm-move").move("h")
-        end,
-      },
-      {
-        "<C-j>",
-        function()
-          require("wezterm-move").move("j")
-        end,
-      },
-      {
-        "<C-k>",
-        function()
-          require("wezterm-move").move("k")
-        end,
-      },
-      {
-        "<C-l>",
-        function()
-          require("wezterm-move").move("l")
-        end,
-      },
-    },
-  },
 }
